ec5925eb35b7cc4543d46ac4ada55809010445ef
Admin <hghg@mail.ru>
2020-12-01 16:43:26 +0300

    rotate scrin

--- app/src/main/java/com/example/animation/views/RobotView.kt
+++ app/src/main/java/com/example/animation/views/RobotView.kt
@@ -5,9 +5,10 @@ import android.content.res.TypedArray
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.LayerDrawable
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.view.View
-import android.widget.ImageView
 import androidx.core.content.res.ResourcesCompat
 import com.example.animation.R
 
@@ -27,19 +28,64 @@ class RobotView(context: Context, attrs: AttributeSet) : View(context, attrs) {
         }
     }
 
+    override fun onSaveInstanceState(): Parcelable? {
+        return SavedState(
+            super.onSaveInstanceState(),
+            robot)
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        val restore = state as SavedState
+        super.onRestoreInstanceState(restore.superState)
+        robot = restore.robot
+
+    }
+
+    class SavedState : BaseSavedState {
+        var robot : Robot
+        constructor(superState: Parcelable?, inputRobot: Robot) : super(superState) {
+            this.robot = inputRobot
+        }
+
+        constructor(superState: Parcel) : super(superState) {
+            this.robot = Robot(0, 0)
+        }
+
+        override fun writeToParcel(out: Parcel?, flags: Int) {
+            super.writeToParcel(out, flags)
+            out?.writeInt(robot.positionX)
+            out?.writeInt(robot.positionY)
+        }
+
+        val CREATOR: Parcelable.Creator<SavedState?> =
+            object : Parcelable.Creator<SavedState?> {
+                override fun createFromParcel(input : Parcel): SavedState {
+                    return SavedState(input)
+                }
+
+                override fun newArray(size: Int): Array<SavedState?> {
+                    return arrayOfNulls<SavedState>(size)
+                }
+            }
+    }
+
     class Robot(
         var positionX: Int,
         var positionY: Int
-    )
+    ) {
+        fun move() {
+            positionX++
+        }
+    }
 
-    private val robot = Robot(0, 0)
+    private var robot = Robot(0, 0)
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
         robot.positionY = height;
     }
 
-    private fun getReflect(d: Drawable) : Drawable {
+    private fun getReflect(d: Drawable): Drawable {
         val array = arrayOf(d)
         return object : LayerDrawable(array) {
             override fun draw(canvas: Canvas) {
@@ -59,7 +105,7 @@ class RobotView(context: Context, attrs: AttributeSet) : View(context, attrs) {
             robot.positionX + 120,
             robot.positionY
         )
-        robot.positionX++
+        robot.move()
         image.draw(canvas)
         if (robot.positionX == width) {
             image = getReflect(image)
